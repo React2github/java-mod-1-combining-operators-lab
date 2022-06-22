2d93e2bae4be94afb48df0a75b6fb235cf9facb9
Abdella <abdella.shukur2@gmail.com>
2022-06-22 17:49:54 -0400

    used instructions to evaluate expressions for lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        // your code here
+        boolean factor = true;
+        boolean factor2 = true;
+        boolean factor3 = false;
+        boolean factor4 = false;
+        System.out.println("Truth table for && operator");
+        System.out.println(factor && factor2);
+        System.out.println(factor && factor3);
+        System.out.println(factor3 && factor4);
+        System.out.println(factor3 && factor2);
+        System.out.println("Truth table for || operator");
+        System.out.println(factor || factor2);
+        System.out.println(factor || factor3);
+        System.out.println(factor3 || factor4);
+        System.out.println(factor3 || factor2);
     }
 }
